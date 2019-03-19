6d8728fd1a8d0caefde16d2e6a4f51067b4349bb
Pawel <p.j.bieniek@wp.pl>
2019-03-19 13:38:22 +0100

    Initial commit, basic reader class constructed

---
+++ src/Reader.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Reader {
+    Scanner reader = new Scanner(System.in);
+
+    public readString(){
+        // return string        
+    }
+
+    public readStringFromUser(){
+        // return strin
+    }
+
+    public readIntFromUser(){
+        // return int
+    }
+
+    public getUserCoordinates(){
+        // return int2
+
+    }
+
+
+
+}
