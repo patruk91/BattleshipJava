97dd9cc358898bd08da74d42959c40c100b3f687
Krzysiek <krzysiekczaja@gmail.com>
2019-03-19 15:29:25 +0100

    Initial commit

---
+++ src/Validator.java
@@ -0,0 +1,25 @@
+public class Validator {
+    public Validator() {
+
+    }
+
+    public boolean validateString(String input) {
+        // check if string is empty
+    }
+
+    public boolean validateStringInRange(String input) {
+        // test if input is of specified value
+    }
+
+    public boolean isNumeric(String input) {
+        // test if input String is of numeric value
+    }
+
+    public boolean isNumberInRange(int start, int end, int input) {
+        // test if int input is in range int start <-> int end
+    }
+
+    public boolean checkCoordinates(String[] coordinates) {
+        // test coordinates given ad 
+    }
+}
