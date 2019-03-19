f43aadd9a4bd6e721343525f5ae2dadb0ec1f050
Pawel <p.j.bieniek@wp.pl>
2019-03-19 13:11:40 +0100

    Player methods declared

--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,27 @@
 public class Player {
-    
-    public void handlePlayer(){
-        //
+    Ocean ocean;
+    String name;
+
+    public Player(String name) {
+        ocean = new Ocean();
+        this.name = name;
     }
+
+    public getName(String name){
+        // ask for user name
+    }
+
+    public getOcean(Ocean ocean){
+        // handle grid data
+    }
+
+    public void shoot(String Coordinates){
+        // ask for coordinates, use Reader class
+        // check if square is ocean or ship
+        // make changes on map
+    }
+
+    public shipPlacement(int x, int y, String diretion, )
+
+    p
 }
