701f3344f6eb58a5383d729bc623df3adb858526
Pawel <p.j.bieniek@wp.pl>
2019-03-19 14:05:36 +0100

    shipPlacement method modified

--- src/Player.java
+++ src/Player.java
@@ -2,17 +2,22 @@ public class Player {
     Ocean ocean;
     String name;
 
-    public Player(String name) {
+    public Player() {
         ocean = new Ocean();
         this.name = name;
+    }
+    public Ocean() {
+        this.ocean = ocean;
     }
 
     public getName(String name){
         // ask for user name
+        return userName;
     }
 
     public getOcean(Ocean ocean){
         // handle grid data
+        return oceanData;
     }
 
     public void shoot(String Coordinates){
@@ -21,7 +26,9 @@ public class Player {
         // make changes on map
     }
 
-    public shipPlacement(int x, int y, String diretion, )
+    public shipPlacement(int x, int y, String diretion, Ocean ocean, String shipName){
+        return ocean;
+    }
 
-    p
+    
 }
