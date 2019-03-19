1021ec29f47910d12df2c2d1480902d340bf85c3
Krzysiek <krzysiekczaja@gmail.com>
2019-03-19 13:17:26 +0100

    Add Ship constructor and metod to check if ship is sink

--- src/Ship.java
+++ src/Ship.java
@@ -5,10 +5,27 @@ public class Ship {
     private List<Square> shipSquares;
 
     Ship(int x, int y, String direction, Ocean ocean, int length) {
+        this.shipSquares = new ArrayList<Square>();
         getShipSquares(x, y, ocean, direction, length);
     }
 
     public void getShipSquares(int x, int y, Ocean ocean, String direction, int shipLength) {
-        this.shipSquares.add(ocean.getSquare(x, y));
+        for (int i = 0; i < shipLength; i++) {
+            this.shipSquares.add(ocean.getSquare(x, y));
+            if (direction.equals("horizontal")) {
+                y++;
+            } else {
+                x++;
+            }
+        }
+    }
+
+    public boolean isDestroyed() {
+        for (Square square: shipSquares) {
+            if (square.getMyStatus().equals("S")) {
+                return false;
+            }
+        }
+        return true;
     }
 }
