9579ec577fecd94635542b677e09c05f9e6650f2
Krzysiek <krzysiekczaja@gmail.com>
2019-03-19 18:56:27 +0100

    Initial commit

---
+++ src/ReaderHelper.java
@@ -0,0 +1,13 @@
+public class ReaderHelper {
+    public ReaderHelper() {
+
+    }
+
+    public String[] splitCoordinates(String input) {
+
+    }
+
+    public int[] convertCoordinates(String[] coordinates) {
+        
+    }
+}
