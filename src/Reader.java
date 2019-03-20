5144b4cbf681bbff20824c69cf76a031f22900c8
Pawel <p.j.bieniek@wp.pl>
2019-03-20 11:43:07 +0100

    getIntFromUser, getStringFromUser method implemented

--- src/Reader.java
+++ src/Reader.java
@@ -1,10 +1,12 @@
 import java.util.*;
 
 public class Reader {
-    Scanner reader = new Scanner(System.in);
-    
+    Scanner reader;
+    Validator validator;
+
     Public Reader(){
         this.reader = new Scanner(System.in);
+        this.validator = new Validator();
     }
 
     private String readString(){
@@ -14,16 +16,40 @@ public class Reader {
 
     }
 
-    public String getStringFromUser(){
-        return userString;
+    public String getStringFromUser(String regex){
+        String userInput = "";
+        boolean answerIsCorrect = false;
+        while (!answerIsCorrect){
+            userInput = this.readString();
+            if (validator.validateString(userInput) && validator.validateStringInRange(userInput, regex)){
+                answerIsCorrect = true;
+            }
+        }
+        return userInput;
     }
 
-    public Integer getIntFromUser(){
+    public Integer getIntFromUser(int start, int end){
+        String userInput = "";
+        int userInteger = 0;
+        boolean answerIsCorrect = false;
+        while (!answerIsCorrect){
+            userInput = this.readString();
+            if (validator.isNumeric(userInput)){
+                userInteger = Integer.parseInt(userInput);
+                if (validator.isNumberInRange(start, end, userInteger)){
+                    answerIsCorrect = true;
+                }
+            }
+        }
         return userInteger;
     }
 
     public Integer getUserCoordinates(){
         return userInteger2;
+
+    }
+
+    public int getMenuOption(int start, int end){
 
     }
 
