4398ab375ee15bbeefb8b4412c4848b1dde4fe37
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-03-20 13:50:39 +0100

    Add handle main option method

--- src/Controller.java
+++ src/Controller.java
@@ -2,6 +2,14 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class Controller {
+    private boolean restartGame;
+
+    public Controller() {
+        this.restartGame = false;
+
+    }
+
+
 
 
     public void runner() {
@@ -10,23 +18,49 @@ public class Controller {
         ControllerHelper controllerHelper = new ControllerHelper();
         Player firstPlayer = new Player();
         Player secondPlayer = new Player();
-        
-        
-
-        view.printMenu();
-        view.printQuestion("Option");
-        reader.getStringFromUser();
 
 
+        while (!this.restartGame) {
+            handleMainOption(reader, view, firstPlayer);
 
 
-        for (Map.Entry<String, Integer> entry : controllerHelper.getShipNamesWithLength().entrySet()) {
+            while (!(firstPlayer.getOcean().isGameOver() || secondPlayer.getOcean().isGameOver())) {
 
+            }
         }
 
     }
 
+    private void handleMainOption(Reader reader, View view, Player firstPlayer) {
+        view.printMenu();
+        view.printQuestion("Option");
+        int option = reader.getIntFromUser(1, 4);
+        switch (option) {
+            case 1:
+                option = 1;
+                view.printGameModes();
+                int gameModes = reader.getIntFromUser(1, 3);
+                view.printQuestion("Choose your game mode");
 
+
+                break;
+            case 2:
+                option = 2;
+                view.printHighscore();
+                break;
+            case 3:
+                view.printCredits();
+                break;
+            case 4:
+                this.restartGame = true;
+                firstPlayer.getOcean().setIsGameOver(true);
+                break;
+
+        }
+    }
+
+    private void handleGameModes() {
 
     }
+
 }
