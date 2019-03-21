dcc40bcadfe1660e051f8602319e5bb38c851ecf
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-03-21 15:01:40 +0100

    Overwrite equals and hash code in coordinates method

--- src/Coordinates.java
+++ src/Coordinates.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Coordinates {
     private int x;
     private int y;
@@ -15,5 +17,17 @@ public class Coordinates {
         return y;
     }
 
-    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinates that = (Coordinates) o;
+        return x == that.x &&
+                y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
 }
