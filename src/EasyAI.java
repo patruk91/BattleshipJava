a139698c6dfb60b9145c09cbee101bce4f911c9e
Krzysiek <krzysiekczaja@gmail.com>
2019-03-21 10:55:55 +0100

    Add simple AI class

---
+++ src/EasyAI.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class EasyAI extends Player {
+    private List<Coordinates> possibleShoots;
+
+    public EasyAI(String name, View viewer) {
+        super(name, viewer);
+        this.possibleShoots = new ArrayList<Coordinates>();
+        this.fillShootsList();
+    }
+
+    private void fillShootsList() {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                this.possibleShoots.add(new Coordinates(i, j));
+            }
+        }
+    }
+
+    public Coordinates getRandomCoordinates(Randomizer randomizer) {
+        int shootListIndex = randomizer.getRandomDigitFromParamter(possibleShoots.size());
+        return this.possibleShoots.get(shootListIndex);
+    }
+}
